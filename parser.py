1b5a4d0f11d9eb9728bb4a6db16cb3e9b1a671a9
miOracle <48694264+miOracle@users.noreply.github.com>
2019-03-25 12:51:25 +0300

    Update parser.py

--- parser.py
+++ parser.py
@@ -21,7 +21,7 @@ if __name__ == "__main__":
     filename = "coverage-error.log"
     regexp = r"\[\d\d\d\d\.\d\d\.\d\d \d\d:\d\d:\d\d\].+"
     lines = read_file(filename)
-    print("Author is d.galamaga")
+    print("Nehaev.Daniil")
     print(filter_lines(regexp, lines))
 =======
 filename = "coverage-error.log"
